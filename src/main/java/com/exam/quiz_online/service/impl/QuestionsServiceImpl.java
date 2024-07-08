0fdfa153b5ae25ce50beeeaaab0912b2e02654a7
quantrinh123 <quantrinh29092003@gmail.com>
2024-07-08 21:22:55 +0700

    frame quizz

---
+++ src/main/java/com/exam/quiz_online/service/impl/QuestionsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.exam.quiz_online.service.impl;
+
+import com.exam.quiz_online.service.QuestionsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionsServiceImpl implements QuestionsService {
+}
